a994189c6fd43216d27c0fa9ce861e130aef1fbf
Federica Mariani <marianif.jobs@gmail.com>
2026-04-08 00:46:20 +0200

    remove watch and wip entries widget

--- targets/watch-widget/expo-target.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
-module.exports = config => ({
-  type: "watch-widget",
-  icon: 'https://github.com/expo.png',
-  colors: { $accent: "darkcyan", },
-  deploymentTarget: "9.4",
-  entitlements: { /* Add entitlements */ },
-});
